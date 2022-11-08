4c6a3f93bf2cfe66a3bfc3ba8cc31a926357924e
Jake Roughton <112968037+JakeRoughton@users.noreply.github.com>
2022-11-07 23:05:23 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 function twoSmallest(arr){
-  let num1 = arr[0];
-  let num2 = arr[1];
-  for(let i=0; i<arr.length;i++){
-    if(arr.length < 1){
+    if(arr.length <= 1){
       return undefined;
-    }
-    else if(arr[i]<num1){
+    } 
+    let num1 = arr[0];
+    let num2 = arr[1];
+    for(let i=0; i<arr.length;i++){
+    if(arr[i]<num1){
       num2=num1;
       num1=arr[i];
-    }else if(i>0 && arr[i]<num2){
+    } else if(i>0 && arr[i]<num2){
       num2=arr[i]
     }
   }
